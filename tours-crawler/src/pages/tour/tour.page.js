62a9a1a959ce361ed3d6f70da3b03f5526240aeb
D1monPokem0n <373305@niuitmo.ru>
2025-04-30 21:05:52 +0300

    Add waitForLoading method in tour page

--- tours-crawler/src/pages/tour/tour.page.js
+++ tours-crawler/src/pages/tour/tour.page.js
@@ -1,5 +1,7 @@
+import { sleep } from 'crawlee';
 import { BasePage } from '../base.page';
 import { Page } from 'playwright';
+import { config } from '../../config';
 
 export class TourPage extends BasePage {
   /**
@@ -8,9 +10,20 @@ export class TourPage extends BasePage {
   constructor(page) {
     this.page = page;
     this.priceTab = this.page.locator('id=tab-prices');
+    this.waitBox = this.page.locator('id="matrix-wait-box');
+    this.filters = this.page.locator('id=matrFil');
+    this.airportRow = this.filters.locator('xpath=.//*[contains(@class, "airport")]');
+    this.airportRowCheckBoxes = this.airportRow.locator('xpath=.//*[@id="checks"]//input');
+    this.arirportRowConfirmBtn = this.airportRow.locator('xpath=.//input[@value="Toepassen"]');
   }
 
   async openPriceTab() {
     await this.priceTab.click();
+  }
+
+  async waitForLoading(waitInterval) {
+    while (await this.waitBox.isVisible()) {
+      await sleep(waitInterval || config.waitInterval);
+    }
   }
 }
