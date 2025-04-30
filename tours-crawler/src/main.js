16885344b2879e3b7a04c66d6449ad1dd4b4b070
D1monPokem0n <373305@niuitmo.ru>
2025-04-30 20:10:47 +0300

    Changed base structure of project

--- tours-crawler/src/main.js
+++ tours-crawler/src/main.js
@@ -1,14 +1,15 @@
-// For more information, see https://crawlee.dev/
-import { PlaywrightCrawler, ProxyConfiguration } from 'crawlee';
+import { PlaywrightCrawler } from 'crawlee';
 import { router } from './routes.js';
 
-const startUrls = ['https://crawlee.dev'];
+const startUrls = [
+  {
+    url: 'https://www.prijsvrij.nl/vakanties/spanje/costa-del-sol/marbella/monarque-sultan',
+    label: 'tour-page',
+  },
+];
 
 const crawler = new PlaywrightCrawler({
-    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
-    requestHandler: router,
-    // Comment this option to scrape the full website.
-    maxRequestsPerCrawl: 20,
+  requestHandler: router,
 });
 
 await crawler.run(startUrls);
