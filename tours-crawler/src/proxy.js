b1e9aa1e6f39ace447c57593c0977b8764ecc927
D1monPokem0n <373305@niuitmo.ru>
2025-05-01 15:53:52 +0300

    Update README.md

--- tours-crawler/src/proxy.js
+++ tours-crawler/src/proxy.js
@@ -1,6 +1,9 @@
 import { ProxyConfiguration } from 'crawlee';
 
-const availableProxyUrls = [[null]];
+const availableProxyUrls = [
+  [null],
+  // ["http://proxy-example.org"]
+];
 
 export const proxyConfiguration = new ProxyConfiguration({
   tieredProxyUrls: availableProxyUrls,
